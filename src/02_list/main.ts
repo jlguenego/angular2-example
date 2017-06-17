37bccc63d708aa342c82f93ce6500e2770bcc3b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-17 14:29:52 +0200

    ngFor

--- src/02_list/main.ts
+++ src/02_list/main.ts
@@ -1,7 +1,3 @@
-
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
