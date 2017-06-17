3f5cb6ca8534e840e2e24b4080426064ea73f0d9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-17 19:46:34 +0200

    add: 04 event databinding

--- src/04_event_databinding/app/app.component.ts
+++ src/04_event_databinding/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { ColorService } from './color.service';
+import { ColorService, IPair } from './color.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app/app.component.html',
-  providers:  [ ColorService ]
+  providers: [ColorService]
 })
 export class AppComponent {
-  public colors: string[];
+  public colors: IPair[];
   constructor(private colorService: ColorService) {
     this.colors = colorService.getColors();
+
+  }
+  public view(color: IPair) {
+    window.alert('color value: ' + color.value);
   }
 
 }
